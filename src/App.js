a72a470bf15086a28eade99d3a16f5d0b20934a1
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-01 22:06:11 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email :</Form.Label>
-            <Form.Control type="email" placeholder="Enter Your Email" />
+            <Form.Control type="email" placeholder="name@example.com" />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
