e19680a1ddc917f88ca1d5d24d3cac10ab454e97
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-01 21:57:00 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ function App() {
   return (
     <>
       <Container fluid="md">
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form>
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>First Name :</Form.Label>
+            <Form.Control type="text" placeholder="Enter First Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Last Name :</Form.Label>
+            <Form.Control type="text" placeholder="Enter Last Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Age :</Form.Label>
+            <Form.Control type="text" placeholder="Enter Your Age" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email :</Form.Label>
+            <Form.Control type="email" placeholder="Enter First Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
       </Container>
     </>
   );
