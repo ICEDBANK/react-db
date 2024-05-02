606983dc73547eff73fdf906aa297a38c08fed4e
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-01 22:03:32 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ function App() {
     <>
       <Container fluid="md">
         <Form>
-          <Form.Group className="mb-3" controlId="">
+          <Form.Group className="mb-3" controlId="firstName">
             <Form.Label>First Name :</Form.Label>
             <Form.Control type="text" placeholder="Enter First Name" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="">
+          <Form.Group className="mb-3" controlId="lastName">
             <Form.Label>Last Name :</Form.Label>
             <Form.Control type="text" placeholder="Enter Last Name" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="">
+          <Form.Group className="mb-3" controlId="age">
             <Form.Label>Age :</Form.Label>
             <Form.Control type="text" placeholder="Enter Your Age" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email :</Form.Label>
-            <Form.Control type="email" placeholder="Enter First Name" />
+            <Form.Control type="email" placeholder="Enter Your Email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
