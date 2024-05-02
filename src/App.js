e77dbad1f02b513c4ebe95bc23df7d642c2cbf34
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-01 22:10:12 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
     <>
-      <Container fluid="md">
-        <Form>
-          <Form.Group className="mb-3" controlId="firstName">
-            <Form.Label>First Name :</Form.Label>
-            <Form.Control type="text" placeholder="Enter First Name" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="lastName">
-            <Form.Label>Last Name :</Form.Label>
-            <Form.Control type="text" placeholder="Enter Last Name" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="age">
-            <Form.Label>Age :</Form.Label>
-            <Form.Control type="text" placeholder="Enter Your Age" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email :</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button variant="primary" type="submit">Submit</Button>
-        </Form>
+      <Container fluid>
+        <Row>
+          <Form>
+            <Form.Group className="mb-3" controlId="firstName">
+              <Form.Label>First Name :</Form.Label>
+              <Form.Control type="text" placeholder="Enter First Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="lastName">
+              <Form.Label>Last Name :</Form.Label>
+              <Form.Control type="text" placeholder="Enter Last Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="age">
+              <Form.Label>Age :</Form.Label>
+              <Form.Control type="text" placeholder="Enter Your Age" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email :</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+          </Form>
+        </Row>
+        <Row>
+
+        </Row>
       </Container>
-      
+
     </>
   );
 }
